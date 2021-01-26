e467b03a796132bae0d907199d30329adfb11228
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-01-26 14:23:04 +0800

    added player, and basic movement functionalities

---
+++ CrabGame/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+                        
+public class Player : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+
+    private NavMeshAgent myAgent;
+
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        //player movement kinopya ko lang itong code sa https://www.youtube.com/watch?v=KU2CKBlCAxQ
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            
+            if(Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
