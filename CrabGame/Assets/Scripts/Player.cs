bf2244af41cddf913b1008b48632cbf2e012f3db
kairo <pyroplex11@gmail.com>
2021-01-26 16:42:54 +0800

    added movement for 2 crabs
    Clicking the crab locks it, clicking anywhere will move it. Click another crab to unlock it and then lock the other crab

--- CrabGame/Assets/Scripts/Player.cs
+++ CrabGame/Assets/Scripts/Player.cs
@@ -7,23 +7,29 @@ public class Player : MonoBehaviour
 {
     public LayerMask whatCanBeClickedOn;
 
-    private NavMeshAgent myAgent;
+    private NavMeshAgent currentCrab;
 
-    private void Start()
+    private void OnMouseDown()
     {
-        myAgent = GetComponent<NavMeshAgent>();
+        // myAgent changed to currentCrab
+        currentCrab = GetComponent<NavMeshAgent>();
+        // If we select a crab, define currentCrab as its NavMesh agent and
+        // change the other crab's currentCrab to null so it doesn't move
+        string otherCrabName = name == "Crab1" ? "Crab2" : "Crab1";
+        GameObject.Find(otherCrabName).GetComponent<Player>().currentCrab = null;
     }
+
     private void Update()
     {
         //player movement kinopya ko lang itong code sa https://www.youtube.com/watch?v=KU2CKBlCAxQ
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && currentCrab != null)
         {
             Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             
             if(Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
             {
-                myAgent.SetDestination(hitInfo.point);
+                currentCrab.SetDestination(hitInfo.point);
             }
         }
     }
