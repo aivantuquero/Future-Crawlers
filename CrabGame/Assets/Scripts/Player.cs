f6a0aa2d66d2698570df1e88d26f99f7b8b6e445
kairo <pyroplex11@gmail.com>
2021-01-29 18:31:53 +0800

    Added temporary stack button
    - Stack button appears only if crabs are near each other.
    - Changed tags for endpoint and platform to "Ground" so we can use touch to move on them
    - Placed endpoint and platform as child of Ground gameobjecct

--- CrabGame/Assets/Scripts/Player.cs
+++ CrabGame/Assets/Scripts/Player.cs
@@ -2,35 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-                        
+
 public class Player : MonoBehaviour
 {
     public LayerMask whatCanBeClickedOn;
+    public GameObject stackbutton;
 
     private NavMeshAgent currentCrab;
+    private Player otherCrabControl;
+    private string otherCrabName;
+    private bool stack = false;
 
-    private void OnMouseDown()
+    // Binded to StackButton button 
+    public void Stack()
     {
-        // myAgent changed to currentCrab
-        currentCrab = GetComponent<NavMeshAgent>();
-        // If we select a crab, define currentCrab as its NavMesh agent and
-        // change the other crab's currentCrab to null so it doesn't move
-        string otherCrabName;
-        
-        if(name == "Crab1")
+        stack = !stack;
+    }
+
+    private void Start()
+    {
+        // Look for other crab
+        if (name == "Crab1")
         {
             otherCrabName = "Crab2";
-        } 
+        }
         else
         {
             otherCrabName = "Crab1";
         }
-        
-        GameObject.Find(otherCrabName).GetComponent<Player>().currentCrab = null;
+        otherCrabControl = GameObject.Find(otherCrabName).GetComponent<Player>();
+    }
+
+    private void OnMouseDown()
+    {
+        currentCrab = GetComponent<NavMeshAgent>();  
+        otherCrabControl.currentCrab = null;
     }
 
     private void Update()
     {
+        // Check if crabs are near each other
+        if ((transform.position - otherCrabControl.transform.position).magnitude < 2f)
+        {
+            // Enable button when crabs are near each other
+            stackbutton.SetActive(true);
+            // Check if stack button is clicked
+            if (stack)
+            {
+                // Code here to stack crabs
+            }
+        }
+        else
+        {
+            // Disable button when crabs are far away from each other
+            stackbutton.SetActive(false);
+        }
+
         //player movement 
         if (Input.GetMouseButtonDown(0) && currentCrab != null)
         {
