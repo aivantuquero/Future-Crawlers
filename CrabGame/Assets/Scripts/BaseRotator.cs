3e4644a467e9ef1a99d2767507c8926aabe57e1c
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-02-01 23:18:42 +0800

    Added camera control script
    Added camera control script  (but not implemented). I also put all actual game objects inside 1 game object

---
+++ CrabGame/Assets/Scripts/BaseRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+           transform.Rotate(0f, -90f*Time.deltaTime, 0f);
+        
+
+    }
+}
