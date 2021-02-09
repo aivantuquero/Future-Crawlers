a94779f4889f6cd5bacebc7faa0d81122f3bca17
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-02-09 15:55:13 +0800

    Improved lighting to imitate clay-like design, added more scenes, and added swipe to rotate

--- CrabGame/Assets/Scripts/BaseRotator.cs
+++ CrabGame/Assets/Scripts/BaseRotator.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class BaseRotator : MonoBehaviour
 {
-    // Update is called once per frame
+
+    //The swipe rotation only works on touch devices, so dapat idebug mo sya sa unity app sa smartphone mo. download mo to https://play.google.com/store/apps/details?id=com.unity3d.mobileremote
+
+    private Touch touch;
+    private Vector2 touchPosition;
+    private Quaternion rotationY;
+
+    //rotation speed modifier
+    private float rotateSpeedModifier = 0.1f;
+
+
     void Update()
     {
-      
-        
-           transform.Rotate(0f, -90f*Time.deltaTime, 0f);
+        //this condition gets activated when you touched a screen (only works on touch devices)
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            //this condition gets activated when it detects a movement on your touch
+            if(touch.phase == TouchPhase.Moved)
+            {
+                //read more about quartenion euler here https://docs.unity3d.com/ScriptReference/Quaternion.Euler.html
+                rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0f);
+                transform.rotation = rotationY * transform.rotation;
+
+            }
+        }
         
 
     }
