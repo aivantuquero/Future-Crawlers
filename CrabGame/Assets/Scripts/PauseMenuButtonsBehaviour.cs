886984c861941cc76bc7b9b08bd2239252f9904c
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-03-17 23:17:05 +0800

    Added a functional pause menu UI

---
+++ CrabGame/Assets/Scripts/PauseMenuButtonsBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtonsBehaviour : MonoBehaviour
+{
+    public void ShowPauseMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(LoadCurrentScene());
+    }
+
+    IEnumerator LoadCurrentScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Prototype");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void MainMenu()
+    {
+        StartCoroutine(LoadMainMenuScene());
+
+    }
+
+    IEnumerator LoadMainMenuScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
