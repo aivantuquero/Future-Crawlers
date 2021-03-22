9637106d414481f74bcf8d6d7188d9886cb5c72a
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-03-22 17:04:21 +0800

    Added Crab Animation and updated the lever ui with animations.

---
+++ CrabGame/Assets/Scripts/BlueCrabAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCrabAnimation : MonoBehaviour
+{
+    public Animator blueCrabAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.hasChanged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            blueCrabAnimator.Play("WalkingBlue");
+            transform.hasChanged = false;
+        }
+        else
+        {
+            blueCrabAnimator.Play("IdleBlue");
+        }
+
+
+    }
+}
---
+++ CrabGame/Assets/Scripts/RedCrabAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCrabAnimation : MonoBehaviour
+{
+
+    public Animator redCrabAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.hasChanged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            redCrabAnimator.Play("RedWalking");
+            transform.hasChanged = false;
+        }
+        else
+        {
+            redCrabAnimator.Play("RedIdle");
+        }
+
+
+    }
+}
