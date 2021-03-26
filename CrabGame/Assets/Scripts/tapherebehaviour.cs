d57794f2ca8b8fb2d3791b4ab4674e02330404b3
Aivan Carlos Tuquero <aivancarlostuquero@gmail.com>
2021-03-26 17:59:00 +0800

    Implemented a functional main menu with animations

---
+++ CrabGame/Assets/Scripts/tapherebehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tapherebehaviour : MonoBehaviour
+{
+    public void levelselect()
+    {
+        StartCoroutine(levelselection());
+    }
+
+    IEnumerator levelselection()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level Selection");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
